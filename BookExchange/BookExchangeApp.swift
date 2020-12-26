ef518a5d72e14a25546915120e4f3a51bac17d2b
Oleg Koptev <olegstankotpev@gmail.com>
2020-12-26 03:05:14 +0300

    Initial Commit

---
+++ BookExchange/BookExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookExchangeApp.swift
+//  BookExchange
+//
+//  Created by Oleg Koptev on 26.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct BookExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+#  HSEBooks iOS Application
+
