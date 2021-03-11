30751e715da9ccc3258e99b13c068291abc85a4a
Oleg Koptev <olegstankoptev@gmail.com>
2021-03-11 17:38:13 +0300

    small changes

---
+++ BookExchange/Views/BookListViews/BookPageTopPart.swift
@@ -0,0 +1,21 @@
+//
+//  BookPageTopPart.swift
+//  BookExchange
+//
+//  Created by Oleg Koptev on 04.02.2021.
+//
+
+import SwiftUI
+
+struct BookPageTopPart: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    var body: some View {
+        Text("")
+    }
+}
+
+struct BookPageNavBar_Previews: PreviewProvider {
+    static var previews: some View {
+        BookPageTopPart()
+    }
+}
