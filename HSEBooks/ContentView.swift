249a135f9738f32283a5083bbdf344cdc21f9f96
Oleg Koptev <olegstankoptev@gmail.com>
2021-03-12 18:12:50 +0300

    Initial Commit

---
+++ HSEBooks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HSEBooks
+//
+//  Created by Oleg Koptev on 12.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HSEBooks/HSEBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSEBooksApp.swift
+//  HSEBooks
+//
+//  Created by Oleg Koptev on 12.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct HSEBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
