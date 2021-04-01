aa8c1d5a93061ca5055ac06ea2df8ad99e390d94
Oleg Koptev <olegstankoptev@gmail.com>
2021-04-01 23:31:15 +0300

    Missing class added

---
+++ HSEBooks/Extensions/RoundedCorner.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedCorner.swift
+//  HSEBooks
+//
+//  Created by Oleg Koptev on 24.03.2021.
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
