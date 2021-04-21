4a70ef67cd42abaff34efd553521a62bb9930552
Oleg Koptev <olegstankoptev@gmail.com>
2021-04-22 01:32:20 +0300

    Initial Commit

---
+++ HSEBooksiOSApp/HSEBooksiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSEBooksiOSAppApp.swift
+//  HSEBooksiOSApp
+//
+//  Created by Oleg Koptev on 22.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct HSEBooksiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
